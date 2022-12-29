b58378aa43d9dfc07e4fedb53ac86e65ec68792e
pasapep <117331219+pasapep@users.noreply.github.com>
2022-12-28 20:10:27 -0600

    Update App.js
    This version is ok. 12/28/2022

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import Breakout from "./Breakout"
 function App() {
 
   const [games, setGames] = useState ([])
-  const [appScores, setAppScores] = useState ([])
 
   useEffect (() => {
     fetch ("http://localhost:6001/games")
@@ -20,8 +19,47 @@ function App() {
     setGames([...games, myGame])
   }
 
-  const addNewScore = (myScore) => {
-    setAppScores([...appScores, myScore.scores])
+  return (
+   <BrowserRouter>
+    <div>
+      <h3> Retro Games </h3>
+      <NavBar /> 
+    </div>
+    <div> 
+      <Switch> 
+        <Route path = "/snakegame">
+          <Snakegame games = {games} addNewGame = {addNewGame} />
+        </Route>
+        <Route path = "/breakout">
+          <Breakout />
+        </Route>
+
+      </Switch>
+    </div>
+   </BrowserRouter>
+  );
+}
+
+export default App;
+import React, {useState, useEffect} from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";// import Header from "./Header";
+import NavBar from "./NavBar"
+import Snakegame from "./Snakegame"
+import Breakout from "./Breakout"
+// import PlantPage from "./PlantPage";
+
+function App() {
+
+  const [games, setGames] = useState ([])
+
+  useEffect (() => {
+    fetch ("http://localhost:6001/games")
+    .then (resp => resp.json())
+    .then (setGames)
+  }, [])
+
+  const addNewGame = (myGame) => {
+    setGames([...games, myGame])
   }
 
   return (
@@ -33,7 +71,50 @@ function App() {
     <div> 
       <Switch> 
         <Route path = "/snakegame">
-          <Snakegame games = {games} addNewGame = {addNewGame} appScores = {appScores} addNewScore = {addNewScore}/>
+          <Snakegame games = {games} addNewGame = {addNewGame} />
+        </Route>
+        <Route path = "/breakout">
+          <Breakout />
+        </Route>
+
+      </Switch>
+    </div>
+   </BrowserRouter>
+  );
+}
+
+export default App;
+import React, {useState, useEffect} from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";// import Header from "./Header";
+import NavBar from "./NavBar"
+import Snakegame from "./Snakegame"
+import Breakout from "./Breakout"
+// import PlantPage from "./PlantPage";
+
+function App() {
+
+  const [games, setGames] = useState ([])
+
+  useEffect (() => {
+    fetch ("http://localhost:6001/games")
+    .then (resp => resp.json())
+    .then (setGames)
+  }, [])
+
+  const addNewGame = (myGame) => {
+    setGames([...games, myGame])
+  }
+
+  return (
+   <BrowserRouter>
+    <div>
+      <h3> Retro Games </h3>
+      <NavBar /> 
+    </div>
+    <div> 
+      <Switch> 
+        <Route path = "/snakegame">
+          <Snakegame games = {games} addNewGame = {addNewGame} />
         </Route>
         <Route path = "/breakout">
           <Breakout />
