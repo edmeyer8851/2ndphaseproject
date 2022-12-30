944c0dbce3c61cfa0b8b04eb3b51083a6e7ed7e0
pasapep <117331219+pasapep@users.noreply.github.com>
2022-12-30 17:08:49 -0600

    Update NavBar.js

--- NavBar.js
+++ NavBar.js
@@ -5,34 +5,34 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Home from "./Home"
+// import './Styles.css';
 
 
 
-function NavBar() {
 
-    //Toggle Button to Switch Color 
-    const [isDarkMode, setIsDarkMode] = useState(true)
+function NavBar( ) {
+
+    const [isDarkMode, setIsDarkMode] = useState(true) 
     const onToggleDarkMode = () => {
-        setIsDarkMode((isDarkMode) => !isDarkMode) 
+        setIsDarkMode((isDarkMode) => !isDarkMode)
     }
-    const textButton = isDarkMode ? "Light Mode" : "Dark Mode"
-
+    const textButton = isDarkMode ? "Light Mode" : "Dark Mode" 
 
     return (
         <div>
-          <Navbar style = {isDarkMode ? {background: "black"} : {background : "yellow"}}>
+        <Navbar style = {isDarkMode ? {background: "black"} : {background : "yellow"}} className = "mx-auto">
         <Container> 
-          <Navbar.Brand href = "/" style = {isDarkMode ? {color: "yellow"} : {color : "black"}}> Home</Navbar.Brand>
-          <Nav className = "justify-content-center mr-auto">
+            <Nav className = "center-navbar"> 
             <Nav.Link href = "/" style = {isDarkMode ? {color: "yellow"} : {color : "black"}}>Home</Nav.Link>
             <Nav.Link href = "/snakegame" style = {isDarkMode ? {color: "yellow"} : {color : "black"}}>Snake Game</Nav.Link>
             <Nav.Link href = "/breakout" style = {isDarkMode ? {color: "yellow"} : {color : "black"}}>Break Out </Nav.Link>
             <Nav.Link href = "/hiscores" style = {isDarkMode ? {color: "yellow"} : {color : "black"}}>High-Score </Nav.Link>  
             <Nav.Link href = "/login" style = {isDarkMode ? {color: "yellow"} : {color : "black"}}>Login </Nav.Link>
-          </Nav>
-        <button onClick = {onToggleDarkMode}>{textButton} </button>
+            </Nav>
+        <button onClick = {onToggleDarkMode}> {textButton}</button>
         </Container>
-      </Navbar>
+        </Navbar>
         </div>
     )
 }
