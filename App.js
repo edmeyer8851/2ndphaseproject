5ffe8865a056a4582d4315e2eca24c9da6e51f1e
pasapep <117331219+pasapep@users.noreply.github.com>
2022-12-30 17:08:15 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,73 +1,58 @@
-import React from "react";
-import styled from 'styled-components';
-import './Styles.css';
-import { Button, Container, Divider, Grid, Header, Image, Menu, Segment } from "semantic-ui-react";
+import React, {useState, useEffect} from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";// import Header from "./Header";
+import NavBar from "./NavBar"
 import { Icon } from 'semantic-ui-react'
+import Snakegame from "./Snakegame"
+import Breakout from "./Breakout"
+import Home from "./Home"
+// import Hiscores from "./Hiscores"
+// import './Styles.css'
+// import Login from "./Login"
 import 'semantic-ui-css/semantic.min.css'
+// import PlantPage from "./PlantPage";
 
-function Home({games}) {
+function App() {
 
-    const allGames = games.map (game => {
-      return game
-    })
-  
-    console.log("allGames", allGames)
-  
-    const selectedArrays = games.sort(() => Math.random() - 0.5).slice(0,5);
-  
-    return (
-  
-      <Grid textAlign = 'center'  verticalAlign = 'middle'>
-        <Header as='h2' color = 'teal'  textAlign = 'center'><br /> Home </Header>
-        <Grid columns = 'equal'>
-          <Grid.Row columns = 'equal'>
-            <Grid.Column>
-              <div className = "center">
-              <div class = "ui card">
-                <div class = "image">
-                    <img src = "https://www.educative.io/v2api/editorpage/6684016653631488/image/5929780181467136" />                
-                </div>
-                <div class = "content"> 
-                    <a class = "header"> Snake Game </a>
-                </div>
-                <div class = "description" textAlign = "center"> Top Snakegame Reviews</div>
-                <div class = "ui list">
-                    {selectedArrays.map((game) => {
-                        if (game.gamename === "snakegame") {
-                            return <div class = "item" key = {game.id}>{game.gamereview} </div>
-                        }
-                    })}
-                </div>
+  const [games, setGames] = useState([])
+  const [scores, setScores] = useState([])
 
-              </div>
-              </div>
-            </Grid.Column>
-            <Grid.Column>
-              <div className = "center">
-              <div class = "ui card">
-                <div class = "image">
-                    <img src = "https://www.educative.io/v2api/editorpage/6684016653631488/image/5929780181467136" />                
-                </div>
-                <div class = "content"> 
-                    <a class = "header"> Breakout Game </a>
-                </div>
-                <div class = "description" textAlign = "center"> Top Breakout Reviews</div>
-                <div class = "ui list">
-                    {selectedArrays.map((game) => {
-                        if (game.gamename === "Breakout") {
-                            return <div class = "item" key = {game.id}>{game.gamereview} </div>
-                        }
-                    })}
-                </div>
+  useEffect (() => {
+    fetch ("http://localhost:6001/games")
+    .then (resp => resp.json())
+    .then (setGames)
+  }, [])
 
-              </div>
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Grid>
-    )
+  const addNewGame = (myGame) => {
+    setGames([...games, myGame])
   }
-  
-  export default Home;
-  
+
+  const addNewScore = (myScore) => {
+    setScores([...scores,myScore])
+  }
+
+
+return (
+  <BrowserRouter> 
+    <div> 
+      <h3> Games </h3>
+      <NavBar  /> 
+    </div>
+    <div> 
+      <Switch> 
+        <Route path = "/snakegame">
+          <Snakegame games = {games} addNewGame = {addNewGame} scores = {scores} addNewScore = {addNewScore} />
+        </Route>
+        <Route path = "/snakegame">
+          <Snakegame games = {games} addNewGame = {addNewGame} scores = {scores} addNewScore = {addNewScore} />
+        </Route>
+        <Route path = "/"> 
+          <Home games = {games} />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+}
+
+export default App;
+
